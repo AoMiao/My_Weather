fb545cc9052ee4a305ce2bf0fcaf89bb0632214d
Administrator <1154225693@qq.com>
2017-02-17 10:59:40 +0800

    数据库模型建立和映射实体类

---
+++ app/src/main/java/com/vince/my_weather/db/County.java
@@ -0,0 +1,46 @@
+package com.vince.my_weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/2/17.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherCode() {
+        return weatherCode;
+    }
+
+    public void setWeatherCode(String weatherCode) {
+        this.weatherCode = weatherCode;
+    }
+}
